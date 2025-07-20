306a7130d1a9280b274e5b4f8e9f30094c2f45aa
Pablo Monteserín <pablomonteserin@pablomonteserin.com>
2025-07-20 15:29:24 +0200

    subida

--- src/application/middlewares/index.js
+++ src/application/middlewares/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import withSockets from './with-sockets';
 
 const Middlewares = (app, io) => {
@@ -7,6 +8,9 @@ const Middlewares = (app, io) => {
     app.use(express.json());
     app.use(withSockets(io));
     app.use(cors()) // Use this after the variable declaration
+    
+    // Serve static files from the public directory
+    app.use(express.static(path.join(process.cwd(), 'public')));
 };
 
 export default Middlewares;
